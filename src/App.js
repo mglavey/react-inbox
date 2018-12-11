4bc3cf9509ac31d8cb9a56c0b3e15fea25e6fbd5
Meg Glavey <glaveym@gmail.com>
2018-12-11 15:10:45 -0600

    still playing around with axios call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import axios from 'axios';
 import Message from './components/Message';
 import Homepage from './components/Homepage';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        messages: []
-        
-    };
-}
-
-async componentDidMount() {
-    const messages = (await axios.get('https://morning-falls-3769.herokuapp.com/api/messages')).data;
-    this.setState({
-        messages,
-    });
-}
 
   render() {
     return (
